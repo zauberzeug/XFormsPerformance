7edcb85d04a2091e5cff7ecf404b30a6b3fa3bb8
Falko Schindler <mail@falkoschindler.de>
2014-10-14 16:55:30 +0200

    initial commit

---
+++ XFormsPerformance/App.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFormsPerformance
+{
+    public static class App
+    {
+        public static DateTime StartTime;
+
+        public static Page GetMainPage()
+        {    
+            return new NavigationPage(new StartPage());
+        }
+    }
+
+    public class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Content = new Button {
+                Text = "Start",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Command = new Command(o => {
+                    App.StartTime = DateTime.Now;
+                    Navigation.PushAsync(new StopPage());
+                }),
+            };
+        }
+    }
+
+    public class StopPage : ContentPage
+    {
+        public StopPage()
+        {
+            Content = new Label {
+                Text = "Stop",
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            (Content as Label).Text += " after " + (DateTime.Now - App.StartTime).TotalMilliseconds + " ms";
+        }
+    }
+}
+
